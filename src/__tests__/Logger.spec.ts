e1e89121474510fb9de9b0f35ebde2bbebeee399
Alex Brazier <aejbrazier.apps@gmail.com>
2020-07-02 20:21:45 +0100

    test: add some tests for Logger

---
+++ src/__tests__/Logger.spec.ts
@@ -0,0 +1,127 @@
+import XHRInterceptor from 'react-native/Libraries/Network/XHRInterceptor';
+import Logger from '../Logger';
+
+jest.mock('react-native/Libraries/Blob/FileReader', () => ({}));
+jest.mock('react-native/Libraries/Network/XHRInterceptor', () => ({
+  isInterceptorEnabled: jest.fn(),
+  setOpenCallback: jest.fn(),
+  setRequestHeaderCallback: jest.fn(),
+  setSendCallback: jest.fn(),
+  setHeaderReceivedCallback: jest.fn(),
+  setResponseCallback: jest.fn(),
+  enableInterception: jest.fn(),
+}));
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('enableXHRInterception', () => {
+  it('should do nothing if interceptor has already been enabled', () => {
+    const logger = new Logger();
+
+    (XHRInterceptor.isInterceptorEnabled as jest.Mock).mockReturnValueOnce(
+      true
+    );
+
+    expect(logger.enableXHRInterception()).toBeUndefined();
+    expect(XHRInterceptor.isInterceptorEnabled).toHaveBeenCalledTimes(1);
+    expect(XHRInterceptor.setOpenCallback).toHaveBeenCalledTimes(0);
+  });
+
+  it('should update the maxRequests if provided', () => {
+    const logger = new Logger();
+
+    logger.enableXHRInterception({ maxRequests: 23 });
+    // @ts-ignore
+    const maxRequests = logger.maxRequests;
+
+    expect(maxRequests).toBe(23);
+  });
+
+  it('should call setOpenCallback', () => {
+    const logger = new Logger();
+    logger.enableXHRInterception();
+
+    expect(XHRInterceptor.setOpenCallback).toHaveBeenCalledTimes(1);
+    expect(XHRInterceptor.setOpenCallback).toHaveBeenCalledWith(
+      expect.any(Function)
+    );
+  });
+
+  it('should call setRequestHeaderCallback', () => {
+    const logger = new Logger();
+    logger.enableXHRInterception();
+
+    expect(XHRInterceptor.setRequestHeaderCallback).toHaveBeenCalledTimes(1);
+    expect(XHRInterceptor.setRequestHeaderCallback).toHaveBeenCalledWith(
+      expect.any(Function)
+    );
+  });
+
+  it('should call setSendCallback', () => {
+    const logger = new Logger();
+    logger.enableXHRInterception();
+
+    expect(XHRInterceptor.setSendCallback).toHaveBeenCalledTimes(1);
+    expect(XHRInterceptor.setSendCallback).toHaveBeenCalledWith(
+      expect.any(Function)
+    );
+  });
+
+  it('should call setHeaderReceivedCallback', () => {
+    const logger = new Logger();
+    logger.enableXHRInterception();
+
+    expect(XHRInterceptor.setHeaderReceivedCallback).toHaveBeenCalledTimes(1);
+    expect(XHRInterceptor.setHeaderReceivedCallback).toHaveBeenCalledWith(
+      expect.any(Function)
+    );
+  });
+
+  it('should call setResponseCallback', () => {
+    const logger = new Logger();
+    logger.enableXHRInterception();
+
+    expect(XHRInterceptor.setResponseCallback).toHaveBeenCalledTimes(1);
+    expect(XHRInterceptor.setResponseCallback).toHaveBeenCalledWith(
+      expect.any(Function)
+    );
+  });
+
+  it('should call enableInterception', () => {
+    const logger = new Logger();
+    logger.enableXHRInterception();
+
+    expect(XHRInterceptor.enableInterception).toHaveBeenCalledTimes(1);
+    expect(XHRInterceptor.enableInterception).toHaveBeenCalledWith();
+  });
+});
+
+describe('getRequests', () => {
+  it('should return the requests', () => {
+    const logger = new Logger();
+
+    // @ts-ignore
+    logger.requests = ['test-request'];
+
+    expect(logger.getRequests()).toEqual(['test-request']);
+  });
+});
+
+describe('clearRequests', () => {
+  it('should clear the requests', () => {
+    const logger = new Logger();
+
+    logger.callback = jest.fn();
+
+    // @ts-ignore
+    logger.requests = ['test-request'];
+
+    logger.clearRequests();
+
+    expect(logger.getRequests()).toEqual([]);
+    expect(logger.callback).toHaveBeenCalledTimes(1);
+    expect(logger.callback).toHaveBeenCalledWith([]);
+  });
+});
