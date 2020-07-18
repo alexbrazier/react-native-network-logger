47b4909b8008795cd05243a6f6e4d57c524cfc76
Alex Brazier <aejbrazier.apps@gmail.com>
2020-07-18 10:20:54 +0100

    test: getRequest

--- src/__tests__/Logger.spec.ts
+++ src/__tests__/Logger.spec.ts
@@ -125,3 +125,38 @@ describe('clearRequests', () => {
     expect(logger.callback).toHaveBeenCalledWith([]);
   });
 });
+
+// Private methods
+
+describe('getRequest', () => {
+  it('should return undefined if missing index', () => {
+    const logger = new Logger();
+    // @ts-expect-error
+    const result = logger.getRequest();
+    expect(result).toBeUndefined();
+  });
+
+  it('should return request that matches index', () => {
+    const logger = new Logger();
+    const requests = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    // @ts-expect-error
+    logger.requests = requests;
+    // @ts-expect-error
+    logger.xhrIdMap = { 0: 0, 1: 1, 2: 2 };
+    // @ts-expect-error
+    const result = logger.getRequest(0);
+    expect(result).toBe(requests[2]);
+  });
+
+  it('should return request that matches index if at start', () => {
+    const logger = new Logger();
+    const requests = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    // @ts-expect-error
+    logger.requests = requests;
+    // @ts-expect-error
+    logger.xhrIdMap = { 0: 0, 1: 1, 2: 2 };
+    // @ts-expect-error
+    const result = logger.getRequest(2);
+    expect(result).toBe(requests[0]);
+  });
+});
