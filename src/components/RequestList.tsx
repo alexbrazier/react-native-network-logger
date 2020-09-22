e1cbe9adef14b5297cb5b54584b98c8f9173e64c
Spencer Yoder <sryoder8@yahoo.com>
2020-09-22 13:27:43 -0500

    feat: Add search box to filter Requests by URL (#28)

--- src/components/RequestList.tsx
+++ src/components/RequestList.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, TextInput, Image } from 'react-native';
 import NetworkRequestInfo from '../NetworkRequestInfo';
-import { useThemedStyles, Theme } from '../theme';
+import { useThemedStyles, Theme, useTheme } from '../theme';
 import ResultItem from './ResultItem';
 import Button from './Button';
 
@@ -17,16 +17,40 @@ const RequestList: React.FC<Props> = ({
   onShowMore,
 }) => {
   const styles = useThemedStyles(themedStyles);
+  const themedColors = useTheme();
+
+  const [searchValue, onChangeSearchText] = useState('');
+  const filterdRequests = requests.filter((request) =>
+    request.url.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
     <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <Image
+          source={require('./search.png')}
+          resizeMode="contain"
+          style={styles.searchIcon}
+        />
+        <TextInput
+          onChangeText={(input) => onChangeSearchText(input)}
+          value={searchValue}
+          placeholder="Filter URLs"
+          underlineColorAndroid={'transparent'}
+          style={styles.textInputSearch}
+          placeholderTextColor={themedColors.colors.muted}
+        />
+      </View>
       <FlatList
-        keyExtractor={(item, index) => index.toString()}
+        keyExtractor={(item, index) =>
+          item.startTime.toString() + index.toString()
+        }
         ListHeaderComponent={() => (
           <Button onPress={onShowMore} style={styles.more}>
             More
           </Button>
         )}
-        data={requests}
+        data={filterdRequests}
         renderItem={({ item }) => (
           <ResultItem request={item} onPress={() => onPressItem(item)} />
         )}
@@ -43,6 +67,26 @@ const themedStyles = (theme: Theme) =>
     },
     more: {
       marginLeft: 10,
+    },
+    searchContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 10,
+      margin: 5,
+      borderWidth: 1,
+      borderColor: theme.colors.muted,
+      borderRadius: 20,
+    },
+    searchIcon: {
+      width: 20,
+      height: 20,
+      marginRight: 10,
+    },
+    textInputSearch: {
+      height: 30,
+      padding: 0,
+      flexGrow: 1,
+      color: theme.colors.text,
     },
   });
 
