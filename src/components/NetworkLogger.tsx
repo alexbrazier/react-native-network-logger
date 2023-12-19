374ec6c8f178e016afdf70c2ba72836fb349f813
jwallet <jose.ouellet@vooban.com>
2023-12-18 19:16:08 -0500

    Debounce new requests changes coming from the callback

--- src/components/NetworkLogger.tsx
+++ src/components/NetworkLogger.tsx
@@ -23,9 +23,7 @@ const sortRequests = (requests: NetworkRequestInfo[], sort: 'asc' | 'desc') => {
 };
 
 const NetworkLogger: React.FC<Props> = ({ theme = 'light', sort = 'desc' }) => {
-  const [requests, setRequests] = useState(
-    sortRequests(logger.getRequests(), sort)
-  );
+  const [requests, setRequests] = useState(logger.getRequests());
   const [request, setRequest] = useState<NetworkRequestInfo>();
   const [showDetails, _setShowDetails] = useState(false);
   const [mounted, setMounted] = useState(false);
@@ -43,7 +41,7 @@ const NetworkLogger: React.FC<Props> = ({ theme = 'light', sort = 'desc' }) => {
 
   useEffect(() => {
     logger.setCallback((updatedRequests: NetworkRequestInfo[]) => {
-      setRequests(sortRequests(updatedRequests, sort));
+      setRequests(updatedRequests);
     });
 
     logger.enableXHRInterception();
@@ -104,8 +102,8 @@ const NetworkLogger: React.FC<Props> = ({ theme = 'light', sort = 'desc' }) => {
   }, [paused, getHar]);
 
   const requestsInfo = useMemo(() => {
-    return requests.map((r) => r.toRow());
-  }, [requests]);
+    return sortRequests(requests, sort).map((r) => r.toRow());
+  }, [sort, requests]);
 
   return (
     <ThemeContext.Provider value={theme}>
---
+++ src/constant.ts
@@ -0,0 +1,3 @@
+// StartNetworkLoggingOptions
+export const LOGGER_MAX_REQUESTS: number = 500;
+export const LOGGER_REFRESH_RATE: number = 50;
