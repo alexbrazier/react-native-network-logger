f83d930404f19ea7a6c29adaf8ec96f2296beda7
Alex Brazier <aejbrazier.apps@gmail.com>
2024-09-24 12:06:28 +0100

    Revert "Update dependencies"
    This reverts commit 869f3aab1e15f49ac2076ce04c6a71d8673219b6.

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,20 +1,18 @@
 const path = require('path');
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
+const { getDefaultConfig } = require('metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const pak = require('../package.json');
+
 const root = path.resolve(__dirname, '..');
 
-const { resolver: defaultResolver } = getDefaultConfig(__dirname);
-
-const defaultConfig = getDefaultConfig(__dirname);
+const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
 
 const modules = Object.keys({
   ...pak.peerDependencies,
 });
 
-const config = {
+module.exports = {
   projectRoot: __dirname,
   watchFolders: [root],
 
@@ -24,8 +22,8 @@ const config = {
     blacklistRE: exclusionList(
       modules.map(
         (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
-      ),
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      )
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
@@ -45,5 +43,3 @@ const config = {
     }),
   },
 };
-
-module.exports = mergeConfig(defaultConfig, config);
