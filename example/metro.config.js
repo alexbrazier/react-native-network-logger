d7c0461f1d0038f5e7ee723c0efb98a4d329fa5a
Alex Brazier <aejbrazier.apps@gmail.com>
2024-10-17 13:34:41 +0100

    Restore comment

--- example/metro.config.js
+++ example/metro.config.js
@@ -23,6 +23,8 @@ const modules = Object.keys({
 const config = {
   projectRoot: __dirname,
   watchFolders: [root],
+  // We need to make sure that only one version is loaded for peerDependencies
+  // So we exclude them at the root, and alias them to the versions in example's node_modules
   resolver: {
     blacklistRE: exclusionList(
       modules.map(
